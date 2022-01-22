4c3d4dd7b3615ef54b0aecbcac5d370f760c8851
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-01-21 17:17:18 -0800

    Update scripts for renaming Chloroplasts.

--- runscripts/01-merge-16S-18S.sh
+++ runscripts/01-merge-16S-18S.sh
@@ -2,14 +2,22 @@
 #activate conda env specified in Colette's repo
 conda activate r-optparse-env
 
+date=211207
+
+#Change taxonomy string to clarify these sequences are derived from Eukaryotes but represent 16S rRNA instead of 18S rRNA
+sed 's/Eukaryota\;/Eukaryota-Chloroplast-16S\;/' 02-PROKs/10-exports/all-16S-seqs.with-tax.tsv > tmp.16S.chloroplast.renamed.tsv
+
 mkdir -p 03-Merged
 
 #run script
 normalizing_16S_18S_tags/normalize_16S_18S_ASVs.R \
-  --inputproks 02-PROKs/10-exports/all-16S-seqs.with-tax.tsv \
+  --inputproks tmp.16S.chloroplast.renamed.tsv \
   --inputeuks 02-EUKs/15-exports/all-18S-seqs.with-PR2-tax.tsv \
-  --outputfile 210420_GA03_GP13 \
+  --outputfile ${date}_GA03_GP13 \
   --bias 5.76
 
 #move output files to subfolder
-mv 210420_GA03_GP13_* 03-Merged/
+mv ${date}_GA03_GP13_* 03-Merged/
+
+#remove temp file
+rm tmp.16S.chloroplast.renamed.tsv
--- runscripts/02-split-cruises.sh
+++ runscripts/02-split-cruises.sh
@@ -1,26 +1,33 @@
 #!/bin/bash -i
 
 mkdir -p 04-Split
+date=211207
+rm 04-Split/${date}*
 
 #rearrange columns to put taxonomy at beginning
-cut -f1,310 03-Merged/210420_GA03_GP13_normalized_sequence_counts.tsv > 04-Split/tmp1
-cut -f2-309 03-Merged/210420_GA03_GP13_normalized_sequence_counts.tsv > 04-Split/tmp2
-paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/210420_GA03_GP13_normalized_sequence_counts_reordered.tsv
+cut -f1,310 03-Merged/${date}_GA03_GP13_normalized_sequence_counts.tsv > 04-Split/tmp1
+cut -f2-309 03-Merged/${date}_GA03_GP13_normalized_sequence_counts.tsv > 04-Split/tmp2
+paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/${date}_GA03_GP13_normalized_sequence_counts_reordered_16S_and_18S.tsv
 rm 04-Split/tmp* 2> /dev/null
 
 #same but for proportions file
-cut -f1,310 03-Merged/210420_GA03_GP13_proportions.tsv > 04-Split/tmp1
-cut -f2-309 03-Merged/210420_GA03_GP13_proportions.tsv > 04-Split/tmp2
-paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/210420_GA03_GP13_proportions_reordered.tsv
+cut -f1,310 03-Merged/${date}_GA03_GP13_proportions.tsv > 04-Split/tmp1
+cut -f2-309 03-Merged/${date}_GA03_GP13_proportions.tsv > 04-Split/tmp2
+paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/${date}_GA03_GP13_proportions_reordered_16S_and_18S.tsv
 rm 04-Split/tmp* 2> /dev/null
 
 #same but now for 16S only
 cut -f1,308 02-PROKs/10-exports/all-16S-seqs.with-tax.tsv > 04-Split/tmp1
 cut -f2-307 02-PROKs/10-exports/all-16S-seqs.with-tax.tsv > 04-Split/tmp2
-paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/211020_GA03_GP13_sequence_counts_reordered.tsv
+paste 04-Split/tmp1 04-Split/tmp2 > 04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv
+
 #remove first line
-tail -n+2 04-Split/211020_GA03_GP13_sequence_counts_reordered.tsv | sponge 04-Split/211020_GA03_GP13_sequence_counts_reordered.tsv
-sed -i 's/#OTU ID/OTU_ID/g' 04-Split/211020_GA03_GP13_sequence_counts_reordered.tsv
+tail -n+2 04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv | sponge 04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv
+sed -i 's/#OTU ID/OTU_ID/g' 04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv
+
+#Change Chloroplast taxonomy string to clarify sequences are derived from Eukaryotes but represent 16S rRNA instead of 18S rRNA
+sed -i 's/Eukaryota\;/Eukaryota-Chloroplast-16S\;/' 04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv
+
 rm 04-Split/tmp* 2> /dev/null
 
 #subset spreadsheets with pandas (as wrapped by a python script; use qiime env for pandas library)
@@ -30,30 +37,30 @@ conda activate qiime2-2019.4
 #plain sequence counts GP13 output
 eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
   sample-metadata-backup/samples-to-remove-GP13.tsv \
-  04-Split/211020_GA03_GP13_sequence_counts_reordered.tsv \
-  04-Split/211020_GP13_sequence_counts_16S_only.tsv
+  04-Split/${date}_GA03_GP13_sequence_counts_reordered_16S-only.tsv \
+  04-Split/${date}_GP13_sequence_counts_16S_only.tsv
 
 #merged spreadsheets (16S + 18S) according to 18S correction factor discussed in README
 #plain sequence counts, GP13 output
 eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
   sample-metadata-backup/samples-to-remove-GP13.tsv \
-  04-Split/210420_GA03_GP13_normalized_sequence_counts_reordered.tsv \
-  04-Split/210420_GP13_normalized_sequence_counts_reordered.tsv
+  04-Split/${date}_GA03_GP13_normalized_sequence_counts_reordered_16S_and_18S.tsv \
+  04-Split/${date}_GP13_normalized_sequence_counts_reordered_16S_and_18S.tsv
 
-#proportions GP13
-eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
-  sample-metadata-backup/samples-to-remove-GP13.tsv \
-  04-Split/210420_GA03_GP13_proportions_reordered.tsv \
-  04-Split/210420_GP13_proportions_reordered.tsv
+#proportions GP13, 16S only
+#eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
+#  sample-metadata-backup/samples-to-remove-GP13.tsv \
+#  04-Split/${date}_GA03_GP13_proportions_reordered_16S-only.tsv \
+#  04-Split/${date}_GP13_proportions_reordered_16S-only.tsv
 
-#sequence counts, GA03 output
-eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
-  sample-metadata-backup/samples-to-remove-GA03.tsv \
-  04-Split/210420_GA03_GP13_normalized_sequence_counts_reordered.tsv \
-  04-Split/210420_GA03_normalized_sequence_counts_reordered.tsv
-
-#proportions, GA03 output
+#sequence counts, GA03 output, merged table
 eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
   sample-metadata-backup/samples-to-remove-GA03.tsv \
-  04-Split/210420_GA03_GP13_proportions_reordered.tsv \
-  04-Split/210420_GA03_proportions_reordered.tsv
+  04-Split/${date}_GA03_GP13_normalized_sequence_counts_reordered_16S_and_18S.tsv \
+  04-Split/${date}_GA03_normalized_sequence_counts_reordered_16S_and_18S.tsv
+
+#proportions, GA03 output, 16S only
+#eASV-pipeline-for-515Y-926R/DADA2-pipeline/02-utility-scripts/remove-bad-columns-and-empty-rows.py \
+#  sample-metadata-backup/samples-to-remove-GA03.tsv \
+#  04-Split/${date}_GA03_GP13_proportions_reordered_16S-only.tsv \
+#  04-Split/${date}_GA03_proportions_reordered_16S-only.tsv
